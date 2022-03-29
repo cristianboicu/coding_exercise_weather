8b3640218223a108fc437aaa1d61607577c0d1d5
Cristian <67103037+cristianboicu@users.noreply.github.com>
2022-03-29 22:06:51 +0300

    Displays details of forecast

--- app/src/main/java/com/cristianboicu/codingexerciseweather/util/BindingAdapter.kt
+++ app/src/main/java/com/cristianboicu/codingexerciseweather/util/BindingAdapter.kt
@@ -1,2 +1,15 @@
 package com.cristianboicu.codingexerciseweather.util
 
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.cristianboicu.codingexerciseweather.data.model.DomainForecast
+
+@BindingAdapter("domainForecast")
+fun TextView.setUserName(item: DomainForecast?) {
+    item?.let {
+        text = "\n\n${it.temp} \n\n" +
+                "Feels like: ${it.feels_like}\n\n" +
+                "${it.weather}\n\n" +
+                "${it.description}"
+    }
+}
