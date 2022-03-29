d05001a6e7611e779b9670cb878e9e6a9250b3c9
Cristian <67103037+cristianboicu@users.noreply.github.com>
2022-03-29 22:15:57 +0300

    Adds navigation on toolbar

--- app/src/main/java/com/cristianboicu/codingexerciseweather/MainActivity.kt
+++ app/src/main/java/com/cristianboicu/codingexerciseweather/MainActivity.kt
@@ -2,12 +2,30 @@ package com.cristianboicu.codingexerciseweather
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+
+private lateinit var appBarConfiguration: AppBarConfiguration
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val navController: NavController = findNavController(R.id.my_nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.my_nav_host_fragment).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
